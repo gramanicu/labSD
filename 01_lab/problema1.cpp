8f86860b5e1be15b203084b18aae3f72d73ce6f2
Grama Nicolae <nicolae.grama@stud.acs.upb.ro>
2019-02-25 13:34:24 +0200

    Rezolvat problema 1

--- 01_lab/problema1.cpp
+++ 01_lab/problema1.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <algorithm>
+
+bool compFuncFirst(std::pair<int, char> a, std::pair<int, char> b) {
+    return (a.first > b.first);
+}
+
+bool compFuncSecond(std::pair<int, char> a, std::pair<int, char> b) {
+    return (a.second > b.second);
+}
 
 int main() {
+    std::pair<int, char> vec[7];
+    vec[0] = std::make_pair(1, 'l');
+    vec[1] = std::make_pair(2, 'm');
+    vec[2] = std::make_pair(3, 'M');
+    vec[3] = std::make_pair(4, 'j');
+    vec[4] = std::make_pair(5, 'v');
+    vec[5] = std::make_pair(6, 's');
+    vec[6] = std::make_pair(7, 'd');
+
+    std::sort(std::begin(vec), std::end(vec), compFuncSecond);
+
+    for(auto v: vec) {
+        std::cout << v.first << " " << v.second << "\n";
+    }
+
+
+
     return 0;
 }
--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@
 # face coding-style automat, la standardul google, cu o mica modificare
 # (4 spatii in loc de 2 la alineate)
 beauty: 
-	clang-format -i -style=file *.c *.h
+	clang-format -i -style=file */*.c */*.h
 
 .PHONY:beauty
